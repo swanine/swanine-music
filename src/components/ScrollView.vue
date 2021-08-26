93cc591147c8c22d67b40959fee26fb5fb698241
swanine <haohao1500@foxmail.com>
2021-08-26 16:27:48 +0800

    滚动组件

---
+++ src/components/ScrollView.vue
@@ -0,0 +1,75 @@
+<template>
+<div id="wrapper" ref="wrapper">
+  <slot>
+  </slot>
+</div>
+</template>
+
+<script>
+import IScroll from 'iscroll/build/iscroll-probe'
+export default {
+  name: 'ScrollView',
+  mounted () {
+    this.iscroll = new IScroll(this.$refs.wrapper, {
+      mouseWheel: true,
+      scrollbars: false,
+      click: true,
+      // 卡顿处理
+      scrollX: false,
+      screenY: true,
+      disablePointer: true,
+      disableTouch: false,
+      disableMouse: true
+    })
+    // 1.创建一个观察者对象
+    /*
+    MutationObserver构造函数只要监听到了指定内容发生了变化, 就会执行传入的回调函数
+    mutationList: 发生变化的数组
+    observer: 观察者对象
+    * */
+    // eslint-disable-next-line prefer-const
+    let observer = new MutationObserver((mutationList, observer) => {
+      // console.log(mutationList)
+      // console.log(this.iscroll.maxScrollY)
+      this.iscroll.refresh()
+      // console.log(this.iscroll.maxScrollY)
+    })
+    // 2.告诉观察者对象我们需要观察什么内容
+    // eslint-disable-next-line prefer-const
+    let config = {
+      childList: true, // 观察目标子节点的变化，添加或者删除
+      subtree: true, // 默认为 false，设置为 true 可以观察后代节点
+      attributeFilter: ['height', 'offsetHeight'] // 观察特定属性
+    }
+    // 3.告诉观察者对象, 我们需要观察谁, 需要观察什么内容
+    /*
+    第一个参数: 告诉观察者对象我们需要观察谁
+    第二个参数: 告诉观察者对象我们需要观察什么内容
+    * */
+    observer.observe(this.$refs.wrapper, config)
+  },
+  methods: {
+    // 提供一个监听滚动距离的方法给外界使用
+    scrolling (fn) {
+      this.iscroll.on('scroll', function () {
+        fn(this.y)
+      })
+    },
+    refresh () {
+      setTimeout(() => {
+        this.iscroll.refresh()
+      }, 100)
+    },
+    scrollTo (x, y, time) {
+      this.iscroll.scrollTo(x, y, time)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#wrapper{
+  width: 100%;
+  height: 100%;
+}
+</style>
