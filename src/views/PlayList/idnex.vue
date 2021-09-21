c571ac7abb35e9c7f17f5814365958ef0d1ff693
swanine <haohao1500@foxmail.com>
2021-09-21 21:41:31 +0800

    feat: playlist tab view

---
+++ src/views/PlayList/idnex.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="song-list-page">
+  <PlayHeader :title="'歌单广场'"/>
+  <TabList/>
+</div>
+</template>
+
+<script>
+import PlayHeader from '@/components/PlayHeader.vue'
+import TabList from '@/components/PlayList/TabList.vue'
+export default {
+  name: 'PlayList',
+  components: {
+    PlayHeader,
+    TabList
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.song-list-page{
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: aliceblue;
+}
+</style>
